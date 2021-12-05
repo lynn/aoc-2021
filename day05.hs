e38bf11b6f679ac9c3f646724201bed295ace51a
Lynn <lynn@foldr.moe>
2021-12-05 22:05:36 +0100

    day 5

--- day05.hs
+++ day05.hs
@@ -3,14 +3,9 @@ import Data.Complex
 import Data.List
 import qualified Data.Map.Strict as M
 
-type Point = (Integer, Integer)
-readPoint :: String -> Point
 readPoint = (\[a,b]->(a,b)) . readInts
-
-isOrthogonal :: (Point, Point) -> Bool
+increment map k = M.insertWith (+) k 1 map
 isOrthogonal ((x1,y1),(x2,y2)) = x1 == x2 || y1 == y2
-
-extent :: (Point, Point) -> [Point]
 extent ((x1,y1),(x2,y2)) =
   let sx = signum (x2-x1)
       sy = signum (y2-y1)
@@ -19,9 +14,9 @@ extent ((x1,y1),(x2,y2)) =
 
 main = do
   txt <- getContents
-  let allVents = [(readPoint p, readPoint q) | [p,_,q] <- map words (lines txt)]
-  let (orthogonal, diagonal) = partition isOrthogonal allVents
-  let add vents tally = foldl' (\acc k -> M.insertWith (+) k 1 acc) tally $ concatMap extent vents
+  let vents = [(readPoint p, readPoint q) | [p,_,q] <- map words $ lines txt]
+  let (orthogonal, diagonal) = partition isOrthogonal vents
+  let add vs tally = foldl' increment tally $ extent =<< vs
   let tally = add orthogonal M.empty
   print $ M.size $ M.filter (>=2) tally
 
