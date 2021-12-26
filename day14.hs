9f8839df7474143d285ed5d3dcc4b50e5bfc2587
Lynn <lynn@foldr.moe>
2021-12-26 18:08:35 +0100

    annotate day 14

--- day14.hs
+++ day14.hs
@@ -1,16 +1,27 @@
+{-# LANGUAGE TupleSections #-}
 import Aoc
 import qualified Data.Map as M
 import Data.List
 
+extent a = maximum a - minimum a
+
 main = do
   [[seed], ruleText] <- getParagraphs
-  let pairs = zip seed (tail seed)
   let rules = M.fromList [((x,y),z) | [[x,y],_,[z]] <- words <$> ruleText]
-  let s0 = M.fromListWith (+) [(p,1) | p <- pairs]
-  let f ((x,y),n) = [((x,p),n), ((p,y),n)] where p = rules M.! (x,y)
-  let step s = M.fromListWith (+) $ f =<< M.assocs s
-  let counts s = M.fromListWith (+) $ (last seed,1) : [(x,n) | ((x,_),n) <- M.toList s]
-  let extent a = maximum a - minimum a
+
+  -- Track counts for each pair.
+  let seedPairs = zip seed (tail seed)
+  let s0 = M.fromListWith (+) $ (,1) <$> seedPairs
+
+  -- An insertion rule like XY -> C means that
+  -- `n` XY pairs give rise to `n` XC pairs and `n` CY pairs.
+  let f ((x,y),n) = [((x,c),n), ((c,y),n)] where c = rules M.! (x,y)
+  let step s = M.fromListWith (+) $ f =<< M.toList s
+
+  -- To get counts for each element, count the left halves of all our pairs,
+  -- then throw in the last element of our original seed.
+  let lefts s = [(x,n) | ((x,_),n) <- M.toList s]
+  let counts s = M.fromListWith (+) $ (last seed,1) : lefts s
   let solve n = extent $ map snd $ M.toList $ counts $ iterate step s0 !! n
   print $ solve 10
   print $ solve 40
